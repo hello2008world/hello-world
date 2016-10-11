173dd8a6d93814949c3a26465c9a36f2b637ef53
unknown <>
2016-10-11 14:27:47 +0800

    Initial commit

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * Created by 崔晓冬 on 2016/10/8.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+
+}
